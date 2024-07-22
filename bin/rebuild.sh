4f5bda11fafc0b679d92602bd2b739157468a558
Sherwin Harris <sherwin.harris@gmail.com>
2024-07-22 10:56:48 -0600

    updated to new images and new docker compose syntax

--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker-compose stop && docker-compose rm -f &&docker-compose build && docker-compose up -d && docker-compose ps
+docker compose stop && docker compose rm -f &&docker compose build && docker compose up -d && docker compose ps
